2aa76ab02f095d87f39bac874c95253ab9466b36
ehmicky <ehmicky@gmail.com>
2020-01-12 19:26:04 +0100

    Fix progress bars tests

--- test/progress.js
+++ test/progress.js
@@ -6,9 +6,14 @@ import sinon from 'sinon'
 
 import { fetchUrl } from './helpers/main.js'
 
+// Tests are not run in an interactive terminal, i.e. progress bars does not
+// show up unless we patch this.
+// eslint-disable-next-line fp/no-mutation
+stderr.isTTY = true
+
 each(
   [
-    ...(stderr.isTTY ? [{ opts: { progress: true }, called: true }] : []),
+    { opts: { progress: true }, called: true },
     { opts: { progress: false }, called: false },
     { called: false },
   ],
@@ -31,18 +36,16 @@ each(
   },
 )
 
-if (stderr.isTTY) {
-  test('Progress bars in parallel', async t => {
-    const spy = sinon.spy(stderr, 'write')
+test('Progress bars in parallel', async t => {
+  const spy = sinon.spy(stderr, 'write')
 
-    await Promise.all(
-      Array.from({ length: 10 }, () =>
-        fetchUrl('index.json', { progress: true }),
-      ),
-    )
+  await Promise.all(
+    Array.from({ length: 10 }, () =>
+      fetchUrl('index.json', { progress: true }),
+    ),
+  )
 
-    t.is(spy.called, true)
+  t.is(spy.called, true)
 
-    spy.restore()
-  })
-}
+  spy.restore()
+})
