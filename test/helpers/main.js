ccf069b3af0d7ffcc17a3bb1dd870da2f73776bc
ehmicky <ehmicky@gmail.com>
2021-06-14 13:52:08 +0200

    Fix self reference in tests

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line node/no-extraneous-import
+import fetchNodeWebsite from 'fetch-node-website'
 import getStream from 'get-stream'
-
-import fetchNodeWebsite from '../../src/main.js'
 
 // Call the main code
 export const fetchReleases = async function (url, opts) {
